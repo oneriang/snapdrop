5737c7d941716a3dc367b9b7edbda9b5a2533c42
oneriang <oneriang@126.com>
2022-11-07 07:56:53 +0000

    update

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -58,7 +58,9 @@ class ServerConnection {
         // hack to detect if deployment or development environment
         const protocol = location.protocol.startsWith('https') ? 'wss' : 'ws';
         const webrtc = window.isRtcSupported ? '/webrtc' : '/fallback';
-        const url = protocol + '://' + location.host + location.pathname + 'server' + webrtc;
+        // const url = protocol + '://' + location.host + location.pathname + 'server' + webrtc;
+        const url = protocol + '://' + `server.yiersan.link/` + 'server' + webrtc;
+        // const url = 'ws' + '://' + `localhost:3000/` + 'server' + webrtc;
         return url;
     }
 
